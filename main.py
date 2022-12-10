16526ece6eaaa17046b12048a56db484ef9942f5
NguyenPhamQuocHuy-20130283 <100356312+NguyenPhamQuocHuy-20130283@users.noreply.github.com>
2022-12-10 21:10:03 +0700

    second

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import cv2
 cascade_src_cars = 'cars.xml'
 video_src = './VidSample\sample_01(Color).mp4'
-cascade_src_two = 'two_wheeler.xml'
 cap = cv2.VideoCapture(video_src)
 
 car_cascade = cv2.CascadeClassifier(cascade_src_cars)
-two_wheeler_cascade = cv2.CascadeClassifier(cascade_src_two)
+
 
 while True:
     ret, img = cap.read()
@@ -17,10 +16,10 @@ while True:
     red = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
     # cars = car_cascade.detectMultiScale(gray, 1.1, 2)
-    bikes = two_wheeler_cascade.detectMultiScale(gray, 1.1, 2)
+    cars = car_cascade.detectMultiScale(gray, 1.1, 2)
     # for (x, y, w, h) in cars:
     #     cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 255), 2)
-    for (x, y, w, h) in bikes:
+    for (x, y, w, h) in cars:
         cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 255), 2)
     cv2.imshow('video', img)
 
